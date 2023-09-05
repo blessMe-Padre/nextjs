dd3d70d9466470c1e8893a71750233bdd0e878cc
Сергей Субботин <zarodiny@yandex.ru>
2023-09-06 09:54:34 +1000

    feat: добавляет fetch

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -6,27 +6,27 @@ async function getData() {
     return response.json();
 }
 
-
 export const metadata = {
     title: 'Блог | Next13 App',
     description: 'Тестовое приложение Next JS 13',
-  }
+}
 
 export default async function Blog() {
+
     const posts = await getData();
 
     return (
-    <>
-    <h1 className="text-3xl font-bold underline text">
-        страница приложения "Блог"
-    </h1>
-    <ul>
-        {posts.map((post) => {
-            <li key={post.id}>
-                <Link href={`/blog/${post.id}`}>{post.title}</Link>
-            </li>
-        })}
-    </ul>
-    </>
+        <>
+            <h1 className="text-3xl font-bold underline text">
+                страница приложения "Блог"
+            </h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link href={`/blog/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ['cyrillic'] })
 
 export const metadata = {
   title: 'Главная | Next13 App',
@@ -13,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body className="bg-color-shark text-color-aliceblue h-screen grid grid-rows-[auto_1fr_auto]">
+      <body className="bg-color-shark text-color-aliceblue h-screen grid grid-rows-[auto_1fr_auto]" >
         <Header />
         <main className='container'>
           {children}
